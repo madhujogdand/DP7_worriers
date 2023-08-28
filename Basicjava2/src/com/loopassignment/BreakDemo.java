bfad3990149302a8e7c60ab3771a3d40aa72f4a4
sanjo <sanjo@Madhu>
2023-08-28 16:27:10 +0530

    ok

---
+++ Basicjava2/src/com/loopassignment/BreakDemo.java
@@ -0,0 +1,15 @@
+package com.loopassignment;
+
+public class BreakDemo {
+	public static void main (String args[]) {
+	     
+		for(int i=0;i<10;i++)
+		{
+			//System.out.println(i);
+		     if(i==5)
+		    	break;//comes out of loop 
+		
+			System.out.println(i);
+		}
+}
+}
